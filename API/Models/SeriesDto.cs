91de00ac2ee26a846bb95d081272be28b4718657
204240 <204240@edu.p.lodz.pl>
2020-03-18 02:18:19 +0100

    added solution Entities with partial model, added dtos to API

---
+++ API/Models/SeriesDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class SeriesDto : VideoDto
+    {
+        public short Season { get; set; }
+
+        public short Episode { get; set; }
+    }
+}
